9b50d322e202865d55864bac4154718ba51b4794
Vukasin Stancevic <stihilus@gmail.com>
2021-07-09 15:43:28 +0200

    small fixes

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,11 @@ import '../App.css';
 function About() {
   return (
     <div>
-      <h1>About Page</h1>
+    <div className="divW50 marginBottom80px">
+        <p className="marginBottom40px">Senior Designer based in Belgrade, Serbia. Currently at <a className="linkUnderline" href="https://www.defisaver.com">DeFi Saver</a>.</p>
+        <p><a className="linkUnderline" href="mailto:stihilus@gmail.com">Contact</a> for freelance opportunities.</p>
+        <div className="marginTop40px"><a className="linkUnderline" href="https://dribbble.com/leftlife">Dribbble</a>, <a className="linkUnderline" href="https://www.behance.net/leftlife">Behance</a></div>
+    </div>
     </div>
   );
 }
